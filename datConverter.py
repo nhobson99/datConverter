353df1957ceb8828e785ad25c367254e9c45783e
Noah Hobson <nhobson@vols.utk.edu>
2020-05-05 13:35:39 -0400

    uploading initial code

---
+++ datConverter.py
@@ -0,0 +1,53 @@
+import sys
+import os
+
+config = open(sys.argv[1])
+
+keep = []
+
+for line in config:
+    if (len(line) > 1):
+        keep.append(line.strip())
+        print(keep[-1])
+
+config.close()
+
+print("\n")
+
+for filename in os.listdir("ingest"):
+    infile = open("ingest/"+filename)
+    outfile = open("output/new."+filename, "w+")
+
+    data = []
+    start = 0
+    indices = []
+
+    for line in infile:
+        if (line == "[Data]\n"):
+            start = 1
+
+        elif (start == 1):
+            items = line.split(',')
+            for i, item in enumerate(items):
+                item = item.strip()
+                if (item in keep):
+                    indices.append(i)
+                    data.append(item)
+            for i in range(len(indices)):
+                outfile.write(data[i])
+                if (i != len(indices)-1):
+                    outfile.write(',')
+            outfile.write('\n')
+            start = 2
+
+        elif (start == 2):
+            items = line.split(',')
+            for i in indices:
+                outfile.write(items[i].strip())
+                if (i != indices[-1]):
+                    outfile.write(',')
+            outfile.write('\n')
+            
+    
+    outfile.close()
+    infile.close()
