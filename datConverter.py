ca7a9048d8b80371a585db2d20175f7897790832
Noah Hobson <nhobson@vols.utk.edu>
2020-05-12 16:42:27 -0400

    safety checks, change output behavior; closes #2

--- datConverter.py
+++ datConverter.py
@@ -1,62 +1,82 @@
 import sys
 import os
 
+# if run without argument, create subdirectories and exit
 if (len(sys.argv) == 1):
-    os.mkdir("ingest")
-    os.mkdir("output")
-    os.mkdir("cache")
+    success = True
+    for dirName in ["cache", "output", "ingest"]:
+        try:
+            os.mkdir(dirName)
+        except:
+            print("Directory " + dirName + " already exists, or permissions denied.")
+            success = False
+    if (success):
+        print("Directories created. Add the command line argument for config.txt to run the program normally.")
     sys.exit(0)
 
+# config.txt, line-separated input
 config = open(sys.argv[1])
 
-keep = []
+keep = []   # names of columns we want to keep
 
 for line in config:
     if (len(line) > 1):
+        # get rid of comments and trailing characters
         keep.append(line.split('#')[0].strip())
-        print(keep[-1])
 
 config.close()
 
-print("\n")
-
+# move all files from ingest into cache
 for filename in os.listdir("ingest"):
     os.rename("ingest/"+filename, "cache/"+filename)
 
 for filename in os.listdir("cache"):
-    infile = open("cache/"+filename)
-    outfile = open("output/new."+filename, "w+")
+    infile =  open("cache/"+filename) # original file
 
-    data = []
-    start = 0
-    indices = []
+    data = {}       # csv-stylilzed data to make life easier
+    dataSize = 0    # number of rows after column headers
+    start = 0       # 0 = [Data], 1 = column headers, 2 = data
+    indices = []    # the indices of names in keep[]
+    names = []      # the names of every column header in this file
 
     for line in infile:
+        # we want everything after the header, starting the line after [Data]
         if (line == "[Data]\n"):
             start = 1
-
+        # first line after [Data] contains the column names
         elif (start == 1):
-            items = line.split(',')
-            for i, item in enumerate(items):
-                item = item.strip()
-                if (item in keep):
+            columns = line.split(',')
+            for i, name in enumerate(columns):
+                name = name.strip()
+                names.append(name)
+                if (name in keep):
                     indices.append(i)
-                    data.append(item)
-            for i in range(len(indices)):
-                outfile.write(data[i])
-                if (i != len(indices)-1):
-                    outfile.write(',')
-            outfile.write('\n')
+                    data[name] = []
             start = 2
-
+        # everything after is data
         elif (start == 2):
-            items = line.split(',')
+            columns = line.split(',')
+            dataSize += 1
             for i in indices:
-                outfile.write(items[i].strip())
-                if (i != indices[-1]):
-                    outfile.write(',')
-            outfile.write('\n')
-            
+                name = names[i]
+                data[name].append(columns[i])
     
-    outfile.close()
-    infile.close()
+    infile.close()  # done with the input file
+
+    # build output file from data
+    outfile = open("output/new."+filename, "w+") # output file
+
+    # print the names of the columns to keep
+    for name in keep:
+        if name in names:
+            outfile.write(name+',')
+    outfile.write('\n')
+
+    # then print their data one at a time
+    for i in range(dataSize):
+        for name in keep:
+            if (name in names):
+                outfile.write(data[name][i]+',')
+        outfile.write('\n')
+
+    outfile.close() # done with the output file
